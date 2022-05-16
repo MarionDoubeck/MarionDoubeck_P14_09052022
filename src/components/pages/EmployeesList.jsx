ab862db469a47cbe171d89c47929f802955ddfa6
Marion Doubeck <mariondoubeck@yahoo.fr>
2022-05-16 11:19:28 +0200

    code organization

--- src/components/pages/EmployeesList.jsx
+++ src/components/pages/EmployeesList.jsx
@@ -6,19 +6,29 @@ import Table from '../utils/Table'
 
 const EmployeesList = () => {
     const dispatch = useDispatch()
+    const loading = useSelector(state => state.isLoading)
+    const error = useSelector(state => state.error)
+    let loadingMessages = (<></>)
     /**
      * load employees list from localStorage to redux store
      * then put this list from the store to the local const employees
      */
      useEffect( () => {
         dispatch(callAPI())
+        if(loading){
+          loadingMessages = (<div>Wait please ...</div>)
+        }else if (error){
+          loadingMessages = (<div>{error}</div>)
+        }
       },[dispatch])
     const employees = useSelector(state => state.employees)
+    
 
   return (
     <div className="employeesList">
         <h2>Current employees : </h2>
         <Table data={employees}/>
+        {loadingMessages}
     </div>
   )
 }
